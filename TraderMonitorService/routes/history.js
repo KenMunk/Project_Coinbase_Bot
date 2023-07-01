5bd21bdf7cb16e2f98aa931674fe9abc1e35e4fa
Kenneth Munk <da.munkk@gmail.com>
2023-07-01 12:11:57 -0700

    Simplified the history request process

--- TraderMonitorService/routes/history.js
+++ TraderMonitorService/routes/history.js
@@ -2,10 +2,7 @@ const converter = require('json-2-csv');
 const express = require('express');
 
 const TargetCrypto = require('../models/TargetCrypto');
-const SMALog = require('../models/smaLog');
-const DiffLog = require('../models/differentialLog');
-const PriceLog = require('../models/pricelog');
-const density = require("../models/PriceDensityLog");
+const TrackerLog = require('../models/trackerLog');
 
 const router = express.Router();
 
@@ -13,80 +10,13 @@ const mongoose = require('mongoose');
 const ObjectID = require('mongodb').ObjectID;
 
 async function aggregateHistoryRows(startTime, targetID){
-	const priceHistory = await PriceLog.find({
+	const priceHistory = await TrackerLog.find({
 		timestamp: {$gte: startTime},
 		TargetCrypto: targetID
 	});
 	
-	var resultArray = [];
 	
-	console.log("Price history found:\n" + priceHistory);
-	
-	if(priceHistory.length>0){
-		for(priceEntry in priceHistory){
-			//First Get the timestamp
-			
-			const priceTime = priceHistory[priceEntry].timestamp;
-			
-			var targetEntry = {
-				timestamp: priceTime,
-				buy: priceHistory[priceEntry].buy,
-				sell: priceHistory[priceEntry].sell,
-			};
-			
-			//Do all of the individual tasks
-			var smaEntries = await SMALog.find({
-				timestamp: priceTime,
-				TargetCrypto: targetID
-			});
-			
-			if(smaEntries.length > 0){
-				
-				for(smaEntry in smaEntries){
-					
-					targetEntry = Object.assign(targetEntry, {[(smaEntries[smaEntry].smaType)]: smaEntries[smaEntry].value});
-					
-				}
-				
-			}
-			
-			var diffEntries = await DiffLog.find({
-				timestamp: priceTime,
-				TargetCrypto: targetID
-			});
-			
-			if(diffEntries.length > 0){
-				for(diffEntry in diffEntries){
-					targetEntry = Object.assign(targetEntry, {[(diffEntries[diffEntry].diffType)]: diffEntries[diffEntry].value});
-				}
-			}
-			
-			var densityEntries = await density.find({
-				timestamp: priceTime,
-				TargetCrypto: targetID
-			});
-			
-			if(densityEntries.length > 0){
-				for(densityEntry in densityEntries){
-					targetEntry = Object.assign(targetEntry, {densityScore: densityEntries[densityEntry].dataScore})
-				}
-			}
-			
-			resultArray.push(targetEntry)
-			
-			console.log("Updating entry with sma entries:\n"+JSON.stringify(resultArray[resultArray.length -1]))
-			
-		}
-		
-		
-		//console.log("Price history with analytics:\n" + resultArray);
-		
-	}
-	else{
-		console.log("Error no history found")
-	}
-	
-	return(resultArray);
+	return(priceHistory);
 }
 
 router.all('/getHistory/:crypto/:currency/:hoursBack', (req, res) => {
