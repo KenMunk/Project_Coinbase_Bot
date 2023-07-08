92e7791c552851cddb1bf71fd4bbe17fe39cc998
Kenneth Munk (KingDel) <contact.me@kenmunk.com>
2023-07-08 16:51:18 -0700

    Update history.js

--- TraderMonitorService/routes/history.js
+++ TraderMonitorService/routes/history.js
@@ -36,66 +36,6 @@ async function aggregateHistoryRows(startTime, targetID, lastUpdate, cryptoTarge
 	return(outputHistory);
 }
 
-router.all('/getHistory/:crypto/:currency/:hoursBack', (req, res) => {
-	
-	//First we get the target crypto id
-	
-	const cryptoString = req.params.crypto+"";
-	const currencyString = req.params.currency+"";
-	
-	console.log("History requested for: " + cryptoString + "-" + currencyString);
-	
-	const timeBack = req.params.hoursBack*60*60000;
-	
-	const timeOfNow = (Date.now().valueOf())
-		
-	TargetCrypto.find({
-		crypto: cryptoString, 
-		currency: currencyString
-	}).then(function(result){
-		if(result.length > 0){
-			
-			const targetID = result[0]._id;
-			const lastUpdate = result[0].lastUpdated;
-			
-			aggregateHistoryRows(
-				timeOfNow-timeBack, 
-				targetID, 
-				lastUpdate, 
-				cryptoString, 
-				currencyString
-			).then(resultHistory => {
-				const jsonCSV = converter.json2csv(resultHistory).then(resultCSV =>
-				{
-					if(resultHistory.length>0){
-						
-						return res.status(200).json({
-							message: "Data available for combo: " + cryptoString + "-"+currencyString+ " when searching with id " + targetID,
-							csvData: resultCSV.replaceAll(/\\n/g,'\r\n'),
-							data: resultHistory
-						});
-					}
-					else{
-						
-						return res.status(200).json({message: "No data available for combo: " + cryptoString + "-"+currencyString+ " when searching with id " + targetID});
-					}
-				});
-				
-			});
-			
-		}
-		else{
-			
-			return res.status(200).json({message: "No data available, save this combo into tracking"});
-			
-		}
-	});
-	
-	//Then we get the price log for the 
-	
-});
-
-
 router.all('/getHistory/:crypto/:currency/:hoursBack/:sampleEvery', (req, res) => {
 	
 	//First we get the target crypto id
@@ -152,5 +92,66 @@ router.all('/getHistory/:crypto/:currency/:hoursBack/:sampleEvery', (req, res) =
 	//Then we get the price log for the 
 	
 });
+
+router.all('/getHistory/:crypto/:currency/:hoursBack', (req, res) => {
+	
+	//First we get the target crypto id
+	
+	const cryptoString = req.params.crypto+"";
+	const currencyString = req.params.currency+"";
+	
+	console.log("History requested for: " + cryptoString + "-" + currencyString);
+	
+	const timeBack = req.params.hoursBack*60*60000;
+	
+	const timeOfNow = (Date.now().valueOf())
+		
+	TargetCrypto.find({
+		crypto: cryptoString, 
+		currency: currencyString
+	}).then(function(result){
+		if(result.length > 0){
+			
+			const targetID = result[0]._id;
+			const lastUpdate = result[0].lastUpdated;
+			
+			aggregateHistoryRows(
+				timeOfNow-timeBack, 
+				targetID, 
+				lastUpdate, 
+				cryptoString, 
+				currencyString,
+				1
+			).then(resultHistory => {
+				const jsonCSV = converter.json2csv(resultHistory).then(resultCSV =>
+				{
+					if(resultHistory.length>0){
+						
+						return res.status(200).json({
+							message: "Data available for combo: " + cryptoString + "-"+currencyString+ " when searching with id " + targetID,
+							csvData: resultCSV.replaceAll(/\\n/g,'\r\n'),
+							data: resultHistory
+						});
+					}
+					else{
+						
+						return res.status(200).json({message: "No data available for combo: " + cryptoString + "-"+currencyString+ " when searching with id " + targetID});
+					}
+				});
+				
+			});
+			
+		}
+		else{
+			
+			return res.status(200).json({message: "No data available, save this combo into tracking"});
+			
+		}
+	});
+	
+	//Then we get the price log for the 
+	
+});
+
 
 module.exports = router;
