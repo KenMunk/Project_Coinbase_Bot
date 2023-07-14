b26d317c3260160d4843d17222f0eaeb815788dd
Kenneth Munk <contact.me@kenmunk.com>
2023-07-13 23:14:20 -0700

    Understanding how the themed components work

--- Frontend/TradeOracleDashboard/components/EditScreenInfo.tsx
+++ Frontend/TradeOracleDashboard/components/EditScreenInfo.tsx
@@ -9,28 +9,28 @@ import { Text, View } from './Themed';
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
     <View>
-      <View style={styles.getStartedContainer}>
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Open up the code for this screen:
-        </Text>
+		<View style={styles.getStartedContainer}>
+			<Text
+			style={styles.getStartedText}
+			lightColor= {Colors.light.font.content.primary}
+			darkColor= {Colors.dark.font.content.primary}>
+				Open up the code for this screen:
+			</Text>
 
-        <View
-          style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
-          darkColor="rgba(255,255,255,0.05)"
-          lightColor="rgba(0,0,0,0.05)">
-          <MonoText>{path}</MonoText>
-        </View>
+			<View
+			style={[styles.codeHighlightContainer, styles.homeScreenFilename]}
+			darkColor={Colors.light.tintedView.primary}
+			lightColor={Colors.light.tintedView.primary}>
+				<MonoText>{path}</MonoText>
+			</View>
 
-        <Text
-          style={styles.getStartedText}
-          lightColor="rgba(0,0,0,0.8)"
-          darkColor="rgba(255,255,255,0.8)">
-          Change any of the text, save the file, and your app will automatically update.
-        </Text>
-      </View>
+			<Text
+			style={styles.getStartedText}
+			lightColor= {Colors.light.font.content.primary}
+			darkColor= {Colors.dark.font.content.primary}>
+				Change any of the text, save the file, and your app will automatically update.
+			</Text>
+		</View>
 
       <View style={styles.helpContainer}>
         <ExternalLink
--- Frontend/TradeOracleDashboard/constants/Colors.ts
+++ Frontend/TradeOracleDashboard/constants/Colors.ts
@@ -2,92 +2,63 @@ const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
 export default {
+	
 	light: {
-		theme: [
-			{
-				text: '#000',
-				background: '#fff',
-				tint: tintColorLight,
-				tabIconDefault: '#ccc',
-				tabIconSelected: tintColorLight,
-				view: {
-					primary: 		"rgba(255,255,255,0.1)",
-					secondary: 		"rgba(255,255,255,0.1)",
-					tertiary: 		"rgba(255,255,255,0.1)",
-				},
-				font: {
-					headings: {
-						primary: 	"rgba(0,0,0,1)",
-						secondary: 	"rgba(255,255,255,0.1)",
-						tertiary: 	"rgba(255,255,255,0.1)",
-					},
-					content: {
-						primary: 	"rgba(255,255,255,0.1)",
-						secondary: 	"rgba(255,255,255,0.1)",
-						tertiary: 	"rgba(255,255,255,0.1)",
-					},
-				}
+		text: 				'#000',
+		background: 		'#fff',
+		tint: 				tintColorLight,
+		tabIconDefault: 	'#ccc',
+		tabIconSelected: 	tintColorLight,
+		view: {
+			primary: 		"rgba(255,255,255,0.1)",
+			secondary: 		"rgba(255,255,255,0.1)",
+			tertiary: 		"rgba(255,255,255,0.1)",
+		},
+		tintedView: {
+			primary: 		"rgba(0,0,0,0.2)",
+			secondary: 		"rgba(0,0,0,0.2)",
+			tertiary: 		"rgba(0,0,0,0.2)",
+		},
+		font: {
+			headings: {
+				primary: 	"rgba(0,0,0,1)",
+				secondary: 	"rgba(0,0,0,1)",
+				tertiary: 	"rgba(0,0,0,1)",
 			},
-		]
+			content: {
+				primary: 	"rgba(0,0,0,1)",
+				secondary: 	"rgba(0,0,0,1)",
+				tertiary: 	"rgba(0,0,0,1)",
+			},
+		}
 		
 	},
 	dark: {
-		text: '#fff',
-		background: '#000',
-		tint: tintColorDark,
-		tabIconDefault: '#ccc',
-		tabIconSelected: tintColorDark,
+		text: 				'#fff',
+		background: 		'#000',
+		tint: 				tintColorDark,
+		tabIconDefault: 	'#ccc',
+		tabIconSelected: 	tintColorDark,
 		view: {
-			primary: [
-				"#eee",
-				"#eee",
-				"#eee"
-			],
-			secondary: [
-				"#eee",
-				"#eee",
-				"#eee"
-			],
-			tertiary: [
-				"#eee",
-				"#eee",
-				"#eee"
-			],
+			primary: 		"#eee",
+			secondary: 		"#eee",
+			tertiary: 		"#eee",
 		},
-		text: {
+		tintedView: {
+			primary: 		"rgba(255,255,255,0.5)",
+			secondary: 		"rgba(255,255,255,0.5)",
+			tertiary: 		"rgba(255,255,255,0.5)",
+		},
+		font: {
 			headings: {
-				primary: [
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)"
-				],
-				secondary: [
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)"
-				],
-				tertiary: [
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)"
-				],
+				primary: 	"rgba(255,255,255,0.1)",
+				secondary: 	"rgba(255,255,255,0.1)",
+				tertiary: 	"rgba(255,255,255,0.1)",
 			},
 			content: {
-				primary: [
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)"
-				],
-				secondary: [
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)"
-				],
-				tertiary: [
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)",
-					"rgba(255,255,255,0.1)"
-				],
+				primary: 	"rgba(255,255,255,0.1)",
+				secondary: 	"rgba(255,255,255,0.1)",
+				tertiary: 	"rgba(255,255,255,0.1)",
 			},
 		}
 	},
