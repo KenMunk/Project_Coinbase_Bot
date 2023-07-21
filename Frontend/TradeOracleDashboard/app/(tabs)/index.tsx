4933cf25bb2d9273e7f7d1ba90ce4a51c746e741
Kenneth Munk <contact.me@kenmunk.com>
2023-07-21 11:49:00 -0700

    Testing the api for the combo list

--- Frontend/TradeOracleDashboard/app/(tabs)/index.tsx
+++ Frontend/TradeOracleDashboard/app/(tabs)/index.tsx
@@ -2,11 +2,18 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View, Background, Content, SidePanel, MenuButton, Title, ButtonLabel} from '../../components/Themed';
 
+import { SideMenu } from '../../components/SideMenu';
+
 import {PageStyles} from '../../styles/PageStyles';
 
 export default function DashboardScreen() {
   return (
     <Background>
+		<SideMenu>
+			<MenuButton>
+				<ButtonLabel>Fetch Test</ButtonLabel>
+			</MenuButton>
+		</SideMenu>
 		<SidePanel>
 			<MenuButton>
 				<ButtonLabel>Menu</ButtonLabel>
--- Frontend/TradeOracleDashboard/components/SideMenu.tsx
+++ Frontend/TradeOracleDashboard/components/SideMenu.tsx
@@ -1,6 +1,29 @@
 import { MenuProps, MenuButton, SidePanel } from './Themed';
 
+async function GetCombos(){
+	
+	try{
+		let comboResponse = await fetch('https://trader-dashboard-service.onrender.com/combos/getList', {
+			method: 'GET'
+		});
+		
+		let comboData = await comboResponse.json();
+		
+		console.log(comboData.data);
+		
+		return(comboData.data);
+	}
+	catch(error){
+		console.log('No Combo Data Found');
+		return([]);
+	}
+	
+}
+
 export function SideMenu(props: MenuProps){
+	
+	const testSideMenu = GetCombos();
+	
 	return(
 		<SidePanel>
 			
